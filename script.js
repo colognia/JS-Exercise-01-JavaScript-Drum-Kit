25700e5ae7dddedb46952be9f3353639b6c7f45b
colognia <frauke.schmidt@protonmail.com>
2023-09-24 11:41:23 -0500

    Change event function
    Eventlistener now listens to keydown, not click.
    Associate key input to keyCode.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const keys = document.getElementsByClassName("keys")[0];
+const body = document.getElementsByTagName("body")[0];
+const soundElement = document.createElement("audio");
 
+
+body.addEventListener("keydown", (event) => {
+    // console.log(event.target.parentElement[`data-key`])
+    console.log(event);
+    // shows keycode
+    if (event.keyCode == "65") {
+        document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+    }
+})
